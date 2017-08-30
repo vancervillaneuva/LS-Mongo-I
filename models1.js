8d8a94413570d571d9076f86be1429877fd5767c
Vance Villanueva <vancervillanueva@gmail.com>
2017-08-29 20:57:38 -0700

    Mongo I complete

---
+++ models1.js
@@ -0,0 +1,21 @@
+const mongoose = require('mongoose');
+
+/* A schema is a description of the format of documents within a
+ * collection. In this case, each Bear is a document of the form:
+ *
+ * {
+ *   species: "American Black Bear",
+ *   latinName: "Ursus americanus",
+ *   createdAt: Mon Aug 14 2017 12:50:16 GMT-0700 (PDT)
+ * }
+ *
+ * Make createdAt default to the current date.
+ */
+const BlogPostSchema = new mongoose.Schema({
+  // TODO: write your schema here
+   blogPostTitle: String,
+   blogPostContent: String,
+   createdAt: { type: Date, default: Date.now },
+});
+
+module.exports = mongoose.model('BlogPosts', BlogPostSchema);
--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const bodyParser = require('body-parser');
 const express = require('express');
 const mongoose = require('mongoose');
 const User = require('./models');
-
+const BlogPost = require('./models1');
 const STATUS_USER_ERROR = 422;
 const STATUS_SERVER_ERROR = 500;
 const server = express();
@@ -73,16 +73,84 @@ server.delete('/users/:id',(req, res) => {
 
 // TODO: write your server code here
 
+// Blog Post GET, POST, and DELETE
+
+server.post('/blogposts', (req,res) => {
+	
+	const {blogPostTitle, blogPostContent} = req.body; 
+
+     if (!blogPostTitle || !blogPostContent) {
+        res.status(422);
+        res.json('missing text or complieted field');
+     return;
+     }
+
+     //const bear = new Bear({species, latinName});
+     const blogPost = new BlogPost(req.body);
+
+    //  bearTypes.save((err) => {
+    //      if (err) throw err;
+    //      res.json(todo);
+    //  });
+   blogPost.save((err, savedBlogPost)=>{
+     if (err) {
+       res.status(500).json({ success: false, message: 'could not save the bear'});
+     } else {
+       res.status(201).json(savedBlogPost);
+     }
+   });
+
+});
+
+server.get('/blogposts', (req, res) => {
+	BlogPost.find({}, (err, data) => {
+        if(err) throw err;
+        res.json(data);
+  });
+      
+// ext Bear.find({/bears})
+
+});
+
+
+server.get('/blogposts/:id', (req, res) => {
+	const { id } = req.params; //? query string
+
+	BlogPost.findById(id, (err, user) => {
+        if (err) throw err;
+          res.json(user);
+	});
+});
+
+
+server.delete('/blogposts/:id',(req, res) => {
+    const { id } = req.params;
+
+    BlogPost.findByIdAndRemove(id, (err, user) => {
+        if (err) throw err;
+        //  User.remove(id).exec();
+        //  User.find({id}).remove().exec();
+          res.json({ success: true, message: 'id successfully deleted'});
+	});
+
+});
 
 
 
 
 
-mongoose.Promise = global.Promise;
+ mongoose.Promise = global.Promise;
 const connect = mongoose.connect(
-  'mongodb://localhost/users',
+  'mongodb://localhost/',
   { useMongoClient: true }
 );
+
+// const connect1 = mongoose.connect(
+//   'mongodb://localhost/blogposts',
+//   { useMongoClient: true }
+// );
+
+
 
 /* eslint no-console: 0 */
 connect.then(() => {
